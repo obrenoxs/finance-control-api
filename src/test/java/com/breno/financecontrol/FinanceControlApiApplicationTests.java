ff0c567b14612e2e113f2b04c88a7cdd5d739d10
Breno <breno.joaosalvadorperez200@gmail.com>
2026-06-16 20:25:11 -0300

    Create project setup

---
+++ src/test/java/com/breno/financecontrol/FinanceControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.financecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
