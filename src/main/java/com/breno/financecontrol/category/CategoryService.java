75123fc3f07132fc503d5e3506b89a085d16a695
Breno <breno.joaosalvadorperez200@gmail.com>
2026-06-17 10:21:11 -0300

    Implement category retrieval operations

--- src/main/java/com/breno/financecontrol/category/CategoryService.java
+++ src/main/java/com/breno/financecontrol/category/CategoryService.java
@@ -1,5 +1,6 @@
 package com.breno.financecontrol.category;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,22 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return obj.orElseThrow(() -> new RuntimeException("Category not found")); //Substituir por ResourceNotFoundException
+	}
+	
 	public Category insert(Category category) {
 		Optional<Category> obj = categoryRepository.findByName(category.getName());
 		
-		if (!obj.isPresent()) {
-			return categoryRepository.save(category);
-		} else {
+		if (obj.isPresent()) {
 			throw new BusinessException("Category already exists");
-		}
+		} 
+			
+		return categoryRepository.save(category);
 	}
 }
