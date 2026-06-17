cd656c5d4a1acc733f3b6bd8daf6ee9cb9c2147a
Breno <breno.joaosalvadorperez200@gmail.com>
2026-06-17 09:35:27 -0300

    Create business exception

---
+++ src/main/java/com/breno/financecontrol/category/CategoryService.java
@@ -0,0 +1,12 @@
+package com.breno.financecontrol.category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+}
