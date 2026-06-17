41e776178d0ffc11e9ba0ba67be248e4f10d5c8c
Breno <breno.joaosalvadorperez200@gmail.com>
2026-06-17 09:59:42 -0300

    Implement category duplicate validation

--- src/main/java/com/breno/financecontrol/category/CategoryService.java
+++ src/main/java/com/breno/financecontrol/category/CategoryService.java
@@ -1,7 +1,11 @@
 package com.breno.financecontrol.category;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.breno.financecontrol.exception.BusinessException;
 
 @Service
 public class CategoryService {
@@ -9,4 +13,13 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	public Category insert(Category category) {
+		Optional<Category> obj = categoryRepository.findByName(category.getName());
+		
+		if (!obj.isPresent()) {
+			return categoryRepository.save(category);
+		} else {
+			throw new BusinessException("Category already exists");
+		}
+	}
 }
