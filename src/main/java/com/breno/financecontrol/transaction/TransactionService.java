c4f09a49cafc83710a563612e1b2a1b3f0744408
Breno <breno.joaosalvadorperez200@gmail.com>
2026-06-17 20:37:48 -0300

    Create Transaction service with business validations

---
+++ src/main/java/com/breno/financecontrol/transaction/TransactionService.java
@@ -0,0 +1,51 @@
+package com.breno.financecontrol.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.breno.financecontrol.category.Category;
+import com.breno.financecontrol.category.CategoryRepository;
+import com.breno.financecontrol.exception.BusinessException;
+import com.breno.financecontrol.exception.ResourceNotFoundException;
+
+@Service
+public class TransactionService {
+
+	@Autowired
+	private TransactionRepository transactionRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public Transaction create(Transaction obj) {
+		Category category = categoryRepository.findById(obj.getCategory().getId())
+				.orElseThrow(() -> new ResourceNotFoundException(obj.getCategory().getId()));
+		
+		obj.setCategory(category);
+		
+		if (obj.getAmount() == null) {
+			throw new BusinessException("Transaction amount is required");
+		}
+		
+		if (obj.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+			throw new BusinessException("Amount must be greater than zero");
+		}
+		
+		if (obj.getDate() == null) {
+			throw new BusinessException("Transaction date is required");
+		}
+		
+		if (obj.getDate().isAfter(LocalDate.now())) {
+			throw new BusinessException("Transaction date cannot be in the future");
+		}
+		
+		if (obj.getType() == null) {
+			throw new BusinessException("Transaction type is required");
+		}
+		
+		return transactionRepository.save(obj);
+	}
+}
